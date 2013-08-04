a1c3d69ba9edb2e0bb25a73262587c37b1a52b2c
Asaf A.M <asaf.ach@gmail.com>
2013-08-04 17:49:41 +0300

    Initial Commit

---
+++ GoodAroundMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoodAroundMe
+//
+//  Created by asaf ahi-mordehai on 8/4/13.
+//  Copyright (c) 2013 GoodAroundMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
